1cf1aa91dfb9bdcba83edc38abbe3dd48a8adf74
John D. Storey <johndangerstorey@gmail.com>
2014-02-12 21:02:02 -0700

    completes assigmnet

--- app/scripts/controllers/message.js
+++ app/scripts/controllers/message.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('chattyApp')
-  .controller('MessageCtrl', function ($scope) {
-    $scope.messages = [];
-  });
+.controller('MessageCtrl', function ($scope, $http) {
+		console.log('Message Ctrl Running')
+  		$http.get('http://localhost:12200/').success(function(messages) {
+  		$scope.messages = messages;
+  	});
+
+  	$scope.addMessage = function() {
+  		console.log("addMessage Function Called");
+  		var newMessage = {message: $scope.newMessageText};
+  		$http.post('http://localhost:12200/', newMessage).success(function(){
+  			$scope.messages.push(newMessage);
+  			$scope.newMessageText = '';
+  		});
+
+  	}
+  	
+  	
+});
---
+++ server.js
@@ -0,0 +1,44 @@
+
+var messages = [{message:'hello'}];
+
+http = require('http');
+http.createServer(function(req, res) {
+  if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Connection': 'close',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    });
+    res.end(JSON.stringify(messages));
+  }
+  if (req.method === 'POST') {
+    var postData = '';
+    req.on('data', function(chunk) {
+      postData += chunk.toString();
+    });
+    req.on('end', function() {
+      console.log(JSON.parse(postData));
+      var msg = JSON.parse(postData);
+      messages.push(msg);
+      res.writeHead(200, {
+        'Connection': 'close',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      });
+      res.end(JSON.stringify(messages));
+    });
+  }
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, {
+        'Connection': 'close',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      });
+    res.end("{}");
+  }
+}).listen(12200);
+
+
+
