7aa80555921608906f95773d33229c98b54a244c
John D. Storey <johndangerstorey@gmail.com>
2014-02-13 19:26:39 -0700

    completes assignment with express

--- server.js
+++ server.js
@@ -1,44 +1,29 @@
 
 var messages = [{message:'hello'}];
 
-http = require('http');
-http.createServer(function(req, res) {
-  if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Connection': 'close',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    });
-    res.end(JSON.stringify(messages));
-  }
-  if (req.method === 'POST') {
-    var postData = '';
-    req.on('data', function(chunk) {
-      postData += chunk.toString();
-    });
-    req.on('end', function() {
-      console.log(JSON.parse(postData));
-      var msg = JSON.parse(postData);
-      messages.push(msg);
-      res.writeHead(200, {
-        'Connection': 'close',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      });
-      res.end(JSON.stringify(messages));
-    });
-  }
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200, {
-        'Connection': 'close',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-      });
-    res.end("{}");
-  }
-}).listen(12200);
+var express = require('express');
 
+var app = express();
 
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept');
+    next();
+  })
+});
 
+app.get('/', function(req, res){
+    res.type('application/json');
+    res.send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res) {
+    messages.push(req.body);
+    res.type('application/json');
+    res.send(req.body);
+});
+
+app.listen(12200);
